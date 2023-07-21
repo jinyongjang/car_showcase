f6b3118e510cb6ea7818a1d0d7860ee889914da0
jinyongjang <ebmdols1@gmail.com>
2023-07-21 14:23:08 +0900

    2023-07-21

---
+++ types/index.ts
@@ -0,0 +1,57 @@
+import { RadioOptionProps } from "@headlessui/react";
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps {
+  title: string;
+  containerStyles?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: "button" | "submit";
+  textStyles?: string;
+  rightIcon?: string;
+  isDisable?: boolean;
+}
+
+export interface SearchManufacturerProps {
+  selected: string;
+  setSelected: (manufacturer: string) => void;
+}
+
+export interface CarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
+}
+
+export interface FilterProps {
+  manufacturer: string;
+  year: string;
+  fuel: string;
+  limit: number;
+  model: string;
+}
+
+export interface OptionProps {
+  title: string;
+  value: string;
+}
+
+export interface CustomFilterProps {
+  title: string;
+  options: OptionProps[];
+  setFilter: (value: string) => void;
+}
+
+export interface ShowMoreProps {
+  pageNumber: number;
+  isNext: boolean;
+  setLimit: (newLimit: number) => void;
+}
